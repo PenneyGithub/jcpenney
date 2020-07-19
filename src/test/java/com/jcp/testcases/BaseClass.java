1368bb43d534300ef6f992abc0d6e64f7375c61f
PenneyGithub <kaashu09@gmail.com>
2020-07-19 16:00:03 +0530

    Logger issue fixed

--- src/test/java/com/jcp/testcases/BaseClass.java
+++ src/test/java/com/jcp/testcases/BaseClass.java
@@ -4,18 +4,18 @@ package com.jcp.testcases;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.core.util.Loader;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeDriverService;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -28,7 +28,7 @@ import com.jcp.utilities.ReadConfig;
 public class BaseClass extends ReadConfig{
 	
 
-			public static WebDriver driver;
+public static WebDriver driver;
 			public static Logger logger;
 			
 			@SuppressWarnings("deprecation")
@@ -55,9 +55,10 @@ public class BaseClass extends ReadConfig{
 					driver = new EdgeDriver(capabilities);
 					
 				}
+			
 				
-			logger= Logger.getLogger("JCPENNEY");
-			PropertyConfigurator.configure("Log4j.properties");
+				logger = Logger.getLogger("jcp");
+				PropertyConfigurator.configure("Log4j.properties");
 			
 			}
 			
